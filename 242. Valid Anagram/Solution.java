7c168cd502d26d2a48c13daeb76caec6fcf53e29
RautuGabrielCosmin <156917324+RautuGabrielCosmin@users.noreply.github.com>
2025-03-16 17:39:03 +0200

    Update Solution.java

--- 242. Valid Anagram/Solution.java
+++ 242. Valid Anagram/Solution.java
@@ -1,16 +1,23 @@
 import java.util.Arrays;
 
-class Solution {
-    public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length())return false;
-        char arr1[]=s.toCharArray();
-        char arr2[]=t.toCharArray();
+public class Solution{
+
+    public boolean validAnagram(String s, String t){
+        if(s.length() != t.length()) return false;
+        char[] arr1 = s.toCharArray();
+        char[] arr2 = t.toCharArray();
         Arrays.sort(arr1);
         Arrays.sort(arr2);
-    
-        if(Arrays.equals(arr1,arr2)){
-            return true;
-        }
+        if(Arrays.equals(arr1, arr2)) return true;
         return false;
+    }
+
+    public static void main(String []args){
+        Solution sol = new Solution();
+        String s = "MUI";
+        String t = "M`";
+        boolean output = sol.validAnagram(s, t);
+        System.out.println(output);
+
     }
 }
