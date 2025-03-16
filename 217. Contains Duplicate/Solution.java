ad7172126c3672b15d643d040eaaa01ba8f72aab
RautuGabrielCosmin <156917324+RautuGabrielCosmin@users.noreply.github.com>
2025-03-16 17:17:56 +0200

    Update Solution.java

--- 217. Contains Duplicate/Solution.java
+++ 217. Contains Duplicate/Solution.java
@@ -1,27 +1,30 @@
 public class Solution{
-
+    
     public boolean containsDuplicate(int[] nums){
-        int temp;
+        int aux;
         for(int i = 1; i < nums.length; i ++){
-            if(nums[i - 1] == nums[i])
+            if(nums[i] == nums[i - 1]){
                 return true;
-        
-        temp = nums[i];
-            for(int j = i - 2; j >=0; j ++){
-                    if(nums[j] == temp)
-                        return true;
-                }
-                nums[i] = nums[i - 1];
-                nums[i - 1] = temp;
+            }
+            aux = nums[i];
+            for(int j = i - 2; j >= 0; j --){
+                if(nums[j] == aux){
+                    return true;
+                }    
+            }
+            nums[i] = nums[i - 1];
+            nums[i - 1] = aux;
         }
         return false;
     }
-
-
-    public static void main(String [] args){
+    
+    public static void main(String []args){
         Solution sol = new Solution();
-        int[] nums = {1,2,3,1};
+        int[]nums = {1,2,3,1};
+        int[]nums1 = {1,2,3,4,5,6,7,8,9};
         boolean out = sol.containsDuplicate(nums);
+        boolean out1 = sol.containsDuplicate(nums1);
         System.out.println(out);
+        System.out.println(out1); 
     }
 }
