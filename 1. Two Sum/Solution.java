815cee475accc7d2852cc94b828f7419b74abbb3
RautuGabrielCosmin <156917324+RautuGabrielCosmin@users.noreply.github.com>
2025-03-17 13:33:33 +0200

    Update Solution.java

--- 1. Two Sum/Solution.java
+++ 1. Two Sum/Solution.java
@@ -1,27 +1,29 @@
 import java.util.Arrays;
 
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int arr[]=new int[2];
-        int i,j;
-        for(i=1;i<nums.length;i++){
-            for(j=i;j<nums.length;j++){
-                if(nums[j]+nums[j-i]==target){
-                    arr[0]=j;
-                    arr[1]=j-i;
+public class Solution{
+
+    public int[] TwoSum(int[] nums, int target){
+        int[] arr = new int[2];
+        if(nums == null || nums.length == 0){
+            return new int[0];
+        }
+        for(int i = 1; i < nums.length; i ++){
+            for(int j = i; j < nums.length; j ++){
+                if(nums[j] + nums[j - i] == target){
+                    arr[0] = j;
+                    arr[1] = j-i;
                     return arr;
                 }
             }
         }
         return arr;
     }
-    public static void main(String [] args)
-    {
-        Solution solution = new Solution();
-        int[] nums1 = {2, 7, 11, 15};
-        int target1 = 9;
-        int[] result1 = solution.twoSum(nums1, target1);
-        System.out.println("Example 1: Input: nums = " + Arrays.toString(nums1) + ", target = " + target1);
-        System.out.println("Output: " + Arrays.toString(result1));
+
+    public static void main(String []args){
+        Solution sol = new Solution();
+        int[] arr = {2,7,11,15};
+        int target = 9;
+        int[] out = sol.TwoSum(arr, target);
+        System.out.println(Arrays.toString(out));
     }
 }
